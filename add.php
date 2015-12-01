76659f90a9874264f232001d7be8360513cc9379
chris netten <200230280@MyGeorgian.ca>
2015-12-01 18:08:32 -0500

    add

--- add.php
+++ add.php
@@ -1,12 +1,11 @@
 <?php 
-	require 'db.php';
+	require('db.php');
 	
 $name=$_POST['name']; 
 $lat=$_POST['lat'];  
 $lng=$_POST['lng']; 
 
-	$qry = "INSERT INTO user ( name, lat, lng)
-VALUES ('$name' , '$lat' , '$lng')";
+	$qry = "INSERT INTO user (name, lat, lng) VALUES ('$name' , '$lat' , '$lng')";
 	
 	$statement = $db_Object->prepare($qry);
 	$statement->execute();
