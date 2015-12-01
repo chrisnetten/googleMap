ceb4a412bd0c94829bdb9047479e984848d76af2
chris netten <200230280@MyGeorgian.ca>
2015-12-01 18:45:46 -0500

    fix

--- index.php
+++ index.php
@@ -65,18 +65,16 @@ $qry='SELECT * FROM user';
           
                  var myLatlng = new google.maps.LatLng( lat, lng);
                          var marker = new google.maps.Marker({
-                         position: myLatlng,
-                         title: name
+                         position: myLatlng
+                         
                           });
                            marker.setMap(map);
-                            marker.addListener('click', toggleBounce);
+                        
           
-          function toggleBounce() {
-  if (marker.getAnimation() !== null) {
-    marker.setAnimation(null);
-  } else {
-    marker.setAnimation(google.maps.Animation.BOUNCE);
-  }
+          var infowindow = new google.maps.InfoWindow({
+    content: name
+  });
+         
           
         
             
