6bd2ee30dac808232330067b12220d40808297ac
chris netten <200230280@MyGeorgian.ca>
2015-12-01 17:56:24 -0500

    add

--- add.php
+++ add.php
@@ -1,13 +1,16 @@
 <?php 
 	require 'db.php';
 	
-	$qry = "INSERT INTO user ( name, lat, lon)
-VALUES ( '$name' , '$lat' ,'$lng')";
+$name=$_POST['name']; 
+$lat=$_POST['lat'];  
+$lng=$_POST['lng']; 
+
+	$qry = "INSERT INTO user ( name, lat, lng)
+VALUES ('$name' , '$lat' , '$lng')";
 	
 	$statement = $db_Object->prepare($qry);
 	$statement->execute();
 	$statement->closeCursor();
-	
 
 include('index.php');
 ?>
