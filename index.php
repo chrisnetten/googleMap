5ffdffacd0177d8b97ccc1f63b9d8b6fc8e249b9
chris netten <200230280@MyGeorgian.ca>
2015-11-30 19:13:15 -0500

    .

--- index.php
+++ index.php
@@ -24,7 +24,43 @@
     <script src="http://maps.googleapis.com/maps/api/js?sensor=false&language=en"></script>
   </head>
   <body>
+      <script>
+          
+        function initializeMap() {
+   
+
+
+        
+          var mapOptions = {
+            zoom: 8,
+            center: new google.maps.LatLng(44.394086, -79.692675),
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+          };
+       var  map = new google.maps.Map(document.getElementById('map'),mapOptions);
+       
+       
+          
+            for(var i = 0; user.length; i++) {
+                    var myLatlng = new google.maps.LatLng( user[i].lat, user[i].lng);
+                         var marker = new google.maps.Marker({
+                         position: myLatlng,
+                         title: user[i].name
+                          });
+                           marker.setMap(map);
+            
+          
+        
+            }
       
+        }
+         
+    
+         
+        google.maps.event.addDomListener(window, 'load', initializeMap);
+        
+
+          
+      </script>
      <div class="jumbotron">
     <div id="map" style="width: 100%; height: 500px;"></div>
     </div>
